b969396a4e7ed7ecab4af0adefb16d6f247c78dc
lea <leahopebotoko@gmail.com>
2025-06-29 09:33:32 +0100

    autres pages.......

--- cleanbox/messagerie/urls.py
+++ cleanbox/messagerie/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
-
-urlpatterns = [
+urlpatterns =[
     path('accueil/', views.accueil, name='accueil'),
     path('connexion/', views.connexion, name='connexion'),
     path('inscription/', views.inscription, name='inscription'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('nouveau_message/', views.nouveau_message, name='nouveau_message'),
-    path('historiques_connexion/', views.nouveau_message, name='historiques_connexion'),
-    
-
+    path('historiques_connexion/', views.historique_connexions, name='historiques_connexion'),
+    path('dashboard/', views.dashboard, name='dashboard'),  # Tableau de bord
+    path('messages_envoyes/', views.messages_envoyes, name='messages_envoyes'),  # Liste Messages envoyés
+    path('fichiers_scannes/', views.fichiers_scannes, name='fichiers_scannes'),  # Historique des fichiers scannés
+    path('parametre_compte/', views.parametres, name='parametres'),  # Page Paramètres du compte
+    path('deconnexion/', views.deconnexion, name='deconnexion'),  # Déconnexion
 ]
+
+    
--- cleanbox/messagerie/views.py
+++ cleanbox/messagerie/views.py
@@ -42,9 +42,21 @@ def nouveau_message(request):
 
 def historique_connexions(request):
     return render(request, 'messagerie/historique_connexions.html')
+def messages_envoyes(request):
+    # Affiche la liste des messages déjà envoyés
+    return render(request,'messagerie/messages_envoyes.html')
 
+def fichiers_scannes(request):
+    # Affiche l'historique des fichiers scannés
+    return render(request, 'messagerie/fichiers_scannes.html')
 
+def parametres(request):
+    # Permet à l'utilisateur de modifier ses paramètres (email, mot de passe, etc.)
+    return render(request, 'messagerie/parametre_compte.html')
 
+def deconnexion(request):
+    # Déconnecte l'utilisateur et redirige vers la page de connexion
+    return redirect('connexion')
 
 
 
